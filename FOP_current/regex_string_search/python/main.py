59139cff8764c1270fd0f5519adcd875ecf470cc
Selva Ganesh M <selvadev2k@gmail.com>
2023-05-19 18:29:43 +0530

    python regex string search

---
+++ FOP_current/regex_string_search/python/main.py
@@ -0,0 +1,15 @@
+import re
+data = input()
+ans = re.search(input(), data, re.I)
+print("String Found") if (ans) else print("String Not Found")
+
+
+
+
+# input description
+# 
+
+
+# I love asgard -> string in which the search must happen.
+
+# Asgard -> string which is the search key
