bc1e2e19cf1aab362b4de853948d7394a2d67b94
Selva Ganesh M <selvadev2k@gmail.com>
2023-06-03 00:50:36 +0530

    recursive bubble sorting with no loops

---
+++ DSA/Algorithms/sorting/no_loop_sort/bubble_sort/python/main.py
@@ -0,0 +1,12 @@
+# def sorting(arr, i=0, j=1, clear=1):
+#   if len(arr)==1: return;
+#   if j==len(arr):
+#     return sorting(arr) if not clear else None;
+#   if (arr[i]>arr[j]):
+#     arr[i], arr[j] = arr[j], arr[i];
+#     clear = 0
+#   return sorting(arr, i+1, j+1, clear)
+
+# arr=[9,8,7,6,5,1,2,3,4,5]
+# sorting(arr)
+# print(arr)
